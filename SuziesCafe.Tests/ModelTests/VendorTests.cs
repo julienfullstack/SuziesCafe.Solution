24e08c1d156ba5d02104c9ef0a96705e9d208f29
Julien L <julienlenaz96@gmail.com>
2023-09-23 23:49:22 -0400

    Passed test for creation of vendor construct

--- SuziesCafe.Tests/ModelTests/VendorTests.cs
+++ SuziesCafe.Tests/ModelTests/VendorTests.cs
@@ -5,5 +5,20 @@ using System;
 
 namespace SuziesCafe.Tests
 {
- 
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test", "test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    
   }
+}
