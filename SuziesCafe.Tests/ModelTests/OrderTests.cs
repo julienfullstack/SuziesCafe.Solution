755593791a1201935170e61c0396b7175395d275
Julien L <julienlenaz96@gmail.com>
2023-09-23 23:03:46 -0400

    Created and passed test for creation of Order object

--- SuziesCafe.Tests/ModelTests/OrderTests.cs
+++ SuziesCafe.Tests/ModelTests/OrderTests.cs
@@ -5,7 +5,23 @@ using System;
 
 namespace SuziesCafe.Tests
 {
+    [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
 
   }
+}
 
   
